641f640de0d0ce20d92f6d7df14c44c7d89b0b79
xilingyuli <786979248@qq.com>
2017-01-27 12:52:13 +0800

    problem1

---
+++ src/P1TwoSum.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by xilingyuli on 2017/1/27.
+ */
+public class P1TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] { map.get(target - nums[i]), i };
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
